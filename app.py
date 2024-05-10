24687558f12fb9ca75fee6abbbf389d8bcda06ac
Sushma Priyanka <78401534+priyankasushma13@users.noreply.github.com>
2024-05-10 15:29:06 +0530

    New Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app=Flask(__name__)
 @app.route("/")
 def hello():
-    return "<h2>Hello World - I am Sushma Priyanka flask deployment lab</h2><hr/>"
+    return "<h2>Hello World - I am Somu Sushma Priyanka flask deployment lab</h2><hr/>"
 
 app.run(host="0.0.0.0",port=5000)
