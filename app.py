ce3ba86d0a3623b84b62f5b94e1c544aa9c0d39b
Sushma Priyanka <78401534+priyankasushma13@users.noreply.github.com>
2024-05-11 18:33:14 +0530

    Update and rename app.py to index.html

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route("/")
-def hello():
-    return "<h2>Hello World - I am Somu Sushma Priyanka flask deployment lab</h2><hr/>"
-
-app.run(host="0.0.0.0",port=5000)
